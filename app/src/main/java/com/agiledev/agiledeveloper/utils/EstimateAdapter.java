90c1a965516ddc11f945598007614910b14e3bb9
Frosti Grétarsson <frg17@hi.is>
2019-03-14 13:38:06 +0000

    Estimation adapter

--- app/src/main/java/com/agiledev/agiledeveloper/utils/EstimateAdapter.java
+++ app/src/main/java/com/agiledev/agiledeveloper/utils/EstimateAdapter.java
@@ -5,9 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.agiledev.agiledeveloper.R;
 import com.agiledev.agiledeveloper.entities.Estimate;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -22,12 +25,19 @@ public class EstimateAdapter extends ArrayAdapter<Estimate> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
+        Estimate es = (Estimate) getItem(position);
 
         if (convertView == null) {
             convertView = LayoutInflater.from(
                     getContext()).inflate(R.layout.item_estimate, parent, false);
         }
 
+        TextView estimateView = (TextView) convertView.findViewById(R.id.estimate_estimate);
+        TextView explanationView = (TextView) convertView.findViewById(R.id.estimate_explanation);
+
+        estimateView.setText(String.valueOf(es.getEstimate()));
+        explanationView.setText(es.getExplanation());
         return convertView;
     }
+
 }
