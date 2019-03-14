49f24e380b7174104ec20feaeece317136577461
goh12 <goh12@hi.is>
2019-03-14 13:59:27 +0000

    fix á priority

--- app/src/main/java/com/agiledev/agiledeveloper/PriorityActivity.java
+++ app/src/main/java/com/agiledev/agiledeveloper/PriorityActivity.java
@@ -3,8 +3,7 @@ package com.agiledev.agiledeveloper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
-
-import com.agiledev.agiledeveloper.utils.PlanningPokerAdapter;
+import com.agiledev.agiledeveloper.utils.PriorityAdapter;
 import com.agiledev.agiledeveloper.utils.UserStoryContainer;
 
 public class PriorityActivity extends AppCompatActivity {
@@ -15,7 +14,7 @@ public class PriorityActivity extends AppCompatActivity {
         setContentView(R.layout.activity_priority);
 
         ListView lv = (ListView) findViewById(R.id.priority_userStoryListView);
-        PlanningPokerAdapter adapter = new PlanningPokerAdapter(this, UserStoryContainer.getUserStories());
+        PriorityAdapter adapter = new PriorityAdapter(this, UserStoryContainer.getUserStories());
         lv.setAdapter(adapter);
     }
 }
